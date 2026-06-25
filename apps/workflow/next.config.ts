59d12de57520e20a570bdbdd6e08a99411274017
Jackson Yeager <jacksonyeager@gmail.com>
2026-06-25 13:26:14 -0400

    Bug fixes

--- apps/workflow/next.config.ts
+++ apps/workflow/next.config.ts
@@ -1,7 +1,6 @@
+import { withWorkflow } from "workflow/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = { transpilePackages: ["@workspace/contract"] };
 
-export default nextConfig;
+export default withWorkflow(nextConfig);
